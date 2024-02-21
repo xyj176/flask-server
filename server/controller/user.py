2719c769a8025c733af99b4693dbd93c43376286
xuyj <xuyj@dist.com.cn>
2024-02-21 17:23:35 +0800

    feat:实现用户表的增删改查

--- server/controller/user.py
+++ server/controller/user.py
@@ -18,24 +18,88 @@ from server.util.response import JsonResponse
 @user_blue.route('/register', methods=['GET', 'POST'])
 def register():
     current_app.logger.info('/user/register')
-    name = request.form.get('name')
-    password = request.form.get('password')
+    # 注册
+    name = request.args.get('name')
+    query = User.query.filter_by(name=name).first()
+    if query is not None:
+        return JsonResponse.success(data='用户名已存在，请重新注册！')
+    password = request.args.get('password')
     user = User(name=name, password=password)
     # 提交到数据库
     db.session.add(user)
     db.session.commit()
-    return JsonResponse.success('register')
+    return JsonResponse.success('注册成功！')
 
 
 # 请求地址：ip+port+/user/login
 @user_blue.route('login')
 def login():
     current_app.logger.info('/user/login')
-    return JsonResponse.success('login')
+    # 登录判断
+    name = request.args.get('name')
+    password = request.args.get('password')
+    user = User.query.filter_by(name=name, password=password).first()
+    if user is None:
+        return JsonResponse.success(data='用户名或密码不匹配，登录失败！')
+    return JsonResponse.success(data='登录成功')
 
 
 # 请求地址：ip+port+/user/modify_pw
 @user_blue.route('/modify_pw')
 def modify_pw():
     current_app.logger.info('/user/modify_pw')
-    return JsonResponse.success('modify_pw')
+    # todo 修改密码
+    name = request.args.get('name')
+    query = User.query.filter_by(name=name).first()
+    if query is None:
+        return JsonResponse.success(data='用户名不存在！')
+    new_password = request.args.get('new_password')
+    if len(new_password) == 0:
+        return JsonResponse.success(data='密码为空，请重新输入！')
+    query.password = new_password
+    db.session.commit()
+    return JsonResponse.success('密码修改成功！')
+
+
+@user_blue.route('/query/<int:id>')
+def query_by_id(id):
+    # 根据主键查询
+    user = User.query.get_or_404(id)
+    return JsonResponse.success(data=user.name)
+
+
+@user_blue.route('/query/all')
+def query_all():
+    # users = User.query.all()
+    # result = '一共有{}条记录'.format(len(users))
+    count = User.query.count()
+    result = '一共有{}条记录'.format(count)
+    return JsonResponse.success(data=result)
+
+
+@user_blue.route('/query/like')
+def query_like():
+    users = User.query.filter(User.name.startswith('x')).all()
+    # users = User.query.filter(User.name.endswith('x')).all()
+    # users = User.query.filter(User.name.contains('x')).all()
+    return JsonResponse.success(data='模糊查询')
+
+
+@user_blue.route('/update')
+def update():
+    user = User.query.filter_by(name='xyj').first()
+    user.password = 'xin'
+    db.session.commit()
+    return JsonResponse.success(data='更新完成')
+
+
+@user_blue.route('/delete')
+def delete():
+    # 查询
+    user = User.query.get(9)
+    # 删除
+    if user is not None:
+        db.session.delete(user)
+    # 同步到数据库
+    db.session.commit()
+    return JsonResponse.success(data='删除成功')
--- server/model/user.py
+++ server/model/user.py
@@ -16,9 +16,7 @@ from server.model.base import BaseModel
 class User(BaseModel):
     __tablename__ = 'user'
 
-    name = db.Column(db.String, comment='用户名')
-    password = db.Column(db.String, comment='密码')
+    name = db.Column(db.String, nullable=False, comment='用户名')
+    password = db.Column(db.String, nullable=False, comment='密码')
     time = db.Column('create_time', db.DateTime, default=datetime.now, comment='创建时间')
     update_time = db.Column('update_time', db.DateTime, default=datetime.now, onupdate=datetime.now, comment='更新时间')
-
-
